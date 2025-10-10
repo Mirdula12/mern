fdb9a72e90e04b947bfaad72d6994551bc77de87
Mirdula12 <mirdulashri@gmail.com>
2025-10-10 15:43:00 +0530

    data

---
+++ script.js
@@ -0,0 +1,47 @@
+/*  console.log("Mirdula");
+
+console.log(a);
+var a=6
+
+console.log(a);
+let a=6;
+
+let a=6;
+let b=a;
+a=4;
+console.log(b);
+
+let object1={age:33};
+let object2=object1;
+object1.age=22;
+console.log(object2.age);
+
+let a=true;
+let b=false;
+if(a||b){
+    console.log("executed");
+} 
+
+console.log(9>>2);
+console.log(9<<2); 
+console.log(9&5);
+console.log(9|5);
+console.log(9^5);
+
+function sum(a,b){
+    return a+b;
+}
+console.log(sum(11,22)); 
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(11,22); */
+
+function sum(a,b){
+    console.log(a+b);
+}
+const sum2=(a,b,c)=>{
+    console.log(a+b+c);
+}
+sum2(11,22,33);
